e4c6763d9b6575e5ba8e4f49476964bf51a976c8
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-03 15:59:25 +0000

    1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-def add(a, b):
- a=1
- b=2
- print(a, " + ", b, " = ", a+b)
- return (a + b)
+from add_0 import add
+
+
+def main():
+    a = 1
+    b = 2
+    print('{} + {} = {}'.format(a, b, add(a, b)))
+
+if __name__ == '__main__':
+    main()
