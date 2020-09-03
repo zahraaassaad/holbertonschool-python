490ed77a00ed67bd069f7ba7944f7ea96221327f
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-03 17:03:16 +0000

    3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    arguments=sys.argv
+    arguments = arguments[1:]
+    result=0
+    for args in arguments:
+        result += int(args)
+            
+    print('{}'.format(result))
+
+if __name__ == '__main__':
+    main()
