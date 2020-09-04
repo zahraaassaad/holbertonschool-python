32b172185deb6fdb63efd370aab6288e80fed5be
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-04 14:23:18 +0000

    other  method 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-import sys
-
-def main():
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    import sys
     args, arg_len = sys.argv, len(sys.argv) - 1
     table = {"+": add, "-": sub, "*": mul, "/": div}
     if arg_len != 3:
@@ -23,19 +22,14 @@ def main():
     arguments = sys.argv
     agruments = arguments[1:]
 
-    if len(arguments) != 3:
+    if len(arguments) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    elif arguments[2] not in "+-*/":
+    elif arguments[2] != "+" and arguments[2] != "-" and arguments[2] != "*" and arguments[2] != "/":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     
-    a, op, b = int(arguments[1]), arguments[2], int(arguments[3])
-    table = {"+": add, "-": sub, "*": mul, "/": div}
-    c = table[arguments[2]](a, b)
-    print("{} {} {} = {}".format(a, op, b, c))
-
     elif arguments[2] == "+":
         print('{} {} {} = {}'.format(arguments[1], arguments[2], arguments[3], format(add(int(arguments[1]), int(arguments[3])))))
 
@@ -47,7 +41,7 @@ def main():
 
     elif arguments[2] == "/":
         print('{} {} {} = {}'.format(arguments[1], arguments[2], arguments[3], format(div(int(arguments[1]), int(arguments[3])))))
-    '''
 
 if __name__ == '__main__':
     main()
+'''
