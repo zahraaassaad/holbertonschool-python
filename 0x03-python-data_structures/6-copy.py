96eeb58465011fa92e61e06c5987540f92ef6bf1
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-07 15:40:21 +0000

    i

--- 0x03-python-data_structures/6-copy.py
+++ 0x03-python-data_structures/6-copy.py
@@ -4,5 +4,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in row:
-            print("{:d}".format(i), end=" " if row[-1] != i else "")
+            print("{:d}".format(i), end = " " if row[-1] != i else "")
         print()
