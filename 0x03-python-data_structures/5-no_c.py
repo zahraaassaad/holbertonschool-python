19ecef5eecd708fea3b2088a3b09979959b91023
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-07 14:16:15 +0000

    if string added

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,5 +5,5 @@ def no_c(my_string):
     if my_string:
         for i in my_string:
             if i != "c" and i != "C":
-                new_string = new_string + i
+                new_string += i
     return new_string
