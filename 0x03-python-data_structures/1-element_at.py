bb6090f0fea695e621371b1531756fbbcf6d57f0
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-07 10:17:30 +0000

    checker

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+
+
 def element_at(my_list, idx):
     if idx > len(my_list) - 1  or idx < 0:
         return None
-    return my_list[idx]
+    else:
+        return my_list[idx]
