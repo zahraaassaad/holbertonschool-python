d77d49346ffddf9e3650a5a9cf2b838aea6ad1d3
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-07 17:07:53 +0000

    uring index

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,22 +2,7 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0:
-        tuple_a = 0, 0
-    elif len(tuple_a) == 1:
-        a,  = tuple_a
-        tuple_a = a, 0
-    if len(tuple_b) == 0:
-        tuple_b = 0, 0
-    elif len(tuple_b) == 1:
-        b,  = tuple_b
-        tuple_b = b, 0
-    a1, a2 = tuple_a
-    b1, b2 = tuple_b
-
-    sum1 = a1 + b1
-    sum2 = a2 + b2
-
-    new_tuple = sum1, sum2
-
-    return new_tuple
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    return (tuple_a[0] + tuple_b[0],
+            tuple_a[1] + tuple_b[1])
