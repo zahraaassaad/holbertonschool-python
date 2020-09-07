4fefa84bd6ba7a505c2f9d353866832dddfe90ee
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-07 15:43:52 +0000

    activated

--- 0x03-python-data_structures/6-copy.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-
-def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for i in row:
-            print("{:d}".format(i), end = " " if row[-1] != i else "")
-        print()
