69409d62450a86da0d099e07cad09aa7a9b5392d
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-07 14:08:21 +0000

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,8 +3,7 @@
 
 def no_c(my_string):
     new_string = ""
-    if my_string:
-        for i in my_string:
-            if i != "c" and i != "C":
-                new_string = new_string + i
+    for i in my_string:
+        if i != "c" and i != "C":
+            new_string = new_string + i
     return new_string
