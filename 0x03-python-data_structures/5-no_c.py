8a4c1641c47eb40d790a274813ee58217606b2be
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-07 14:39:44 +0000

    join used instead of +=

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,4 @@
 
 
 def no_c(my_string):
-    new_string = ""
-    for i in my_string:
-        if i != "c" and i != "C":
-            new_string += i
-    return new_string
+    return ''.join(i for i in my_string if i not in 'cC')
