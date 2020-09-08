fbd7069d631be828257162cdfe062fee62aac9e4
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-08 14:13:23 +0000

    return function with condition

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,11 +2,4 @@
 
 
 def search_replace(my_list, search, replace):
-    if my_list:
-        new_list = list(my_list)
-        for i in range(len(new_list)):
-            if new_list[i] == search:
-                new_list[i] = replace
-            else:
-                new_list[i] = new_list[i]
-    return new_list
+    return [replace if i == search else i for i in my_list]
