5e8baa63b10363b4006f56470f752ce757ff92dd
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-10 09:49:53 +0000

    0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i=0
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end = "" if i != x - 1 else "\n")
+    except:
+        print()
+        return i
+    return i+1
