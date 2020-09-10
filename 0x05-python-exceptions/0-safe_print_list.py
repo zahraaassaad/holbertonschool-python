70bdfc30f81a4d86b1497bdc1e091f5eb9432120
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-10 09:51:27 +0000

    remove if condition in print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,9 @@ def safe_print_list(my_list=[], x=0):
     i=0
     try:
         for i in range(x):
-            print("{}".format(my_list[i]), end = "" if i != x - 1 else "\n")
+            print("{}".format(my_list[i]), end = "") # if i != x - 1 else "\n")
     except:
         print()
         return i
+    print()
     return i+1
