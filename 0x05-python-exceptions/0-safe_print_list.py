7485424b285bb39c46b5e2acc1430100d0630a1c
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-10 11:10:13 +0000

    ind -> x

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@
 
 def safe_print_list(my_list=[], x=0):
     i = 0
-    while index ind > 0:
-        ind -= 1
+    while x > 0:
+        x -= 1
         try:
             print("{}".format(my_list[i]), end="")
             i += 1
