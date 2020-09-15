e53aaf60da2f642fe53d6f9dec5108b76de96f94
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-15 09:36:05 +0000

    reduced comments

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 """
     3-square.py
-    Module defining square by private instance attribute size
-    raises exception if size < 0 or size non integer
+    Module defining square
     return size
 """
 
 
 class Square:
-    """Square class.
-       It defines a square by private instance attribute size.
+    """Square class. It defines a square.
        return size
     """
     def __init__(self, size=0):
