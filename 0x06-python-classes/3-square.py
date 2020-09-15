c207d58e905bb148a9ff55951a219b6e70079d8d
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-15 09:11:57 +0000

    comments

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -2,14 +2,16 @@
 """
     3-square.py
     Module defining square by private instance attribute size
-    raises exception if size < 0 or size non integer includes public instance method area(): calculates the area of the square
+    raises exception if size < 0 or size non integer
+    includes public instance method area(): calculates the area of the square
     return {}
 """
 
 
 class Square:
     """Square class.
-       It defines a square by private instance attribute size. It calculates the area by area() function.
+       It defines a square by private instance attribute size. 
+       It calculates the area by area() function.
     """
     def __init__(self, size=0):
         """Initializes the data."""
@@ -20,5 +22,4 @@ class Square:
         self.__size = size
     
     def area(self):
-        """Calculates the area of square."""
         return (self.__size) * (self.__size)
