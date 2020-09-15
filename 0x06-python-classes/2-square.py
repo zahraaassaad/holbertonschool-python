08ad41805f84fd694d2009d8357d141f4f796a99
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-15 08:12:56 +0000

    2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -15,13 +15,6 @@ class Square:
         """Initializes the data."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        #try:
-        #    size >=0
-        #    self.__size = size
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        #except TypeError:
-        #    print("size must be an integer")
-        #except ValueError:
-        #    print("size must be >= 0")
