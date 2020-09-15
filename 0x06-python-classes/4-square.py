d83072947ba4a8eeffd03a0f0369a7448b48284e
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-15 12:12:16 +0000

    add line

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,37 +4,6 @@
     Module defining square
     return {}
 """
-class Square:
-    """Class Square"""
-
-    def __init__(self, size=0):
-        """method to initiate"""
-        self.size = size
-
-    @property
-    def size(self):
-        """Getter method to retrieve"""
-        return (self.__size)
-
-    @size.setter
-    def size(self, value):
-        """Setter method to initalize value"""
-        if type(value) != int:
-            raise TypeError('size must be an integer')
-        if value < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = value
-
-    def area(self):
-        """class method to return current square area"""
-        return (self.__size**2)
-'''
-#!/usr/bin/python3
-"""
-    4-square.py
-    Module defining square
-    return {}
-"""
 
 
 class Square:
@@ -64,4 +33,4 @@ class Square:
     def area(self):
         """Return the current square area."""
         return self.__size ** 2
-'''
+
