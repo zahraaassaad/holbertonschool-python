8065c23bbd5470e61ee5886b7687b210ad69fa8b
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-15 08:57:13 +0000

    added comments before function

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -21,5 +21,8 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
     
+    """area function.
+       calculates area of square.
+    """
     def area(self):
         return (self.__size) * (self.__size)
