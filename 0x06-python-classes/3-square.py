aa1bc582b94831ea62e20952ca23376be241893c
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-15 08:21:30 +0000

    3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    3-square.py
+    Module defining square by private instance attribute size
+    raises exception if size < 0 or size non integer
+    includes public instance method area() that calculates the area of the square
+    return {}
+"""
+
+
+class Square:
+    """Square class.
+       It defines a square by private instance attribute size
+       It calculates the area by area() function
+    """
+    def __init__(self, size=0):
+        """Initializes the data."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
+    def area(self):
+        return self.__size**2
