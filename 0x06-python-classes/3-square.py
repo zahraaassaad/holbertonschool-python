c41031a1e942f7dae0c8329cdfa591bfd9cc9416
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-15 09:41:25 +0000

    3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,22 +1,4 @@
 #!/usr/bin/python3
-
-
-class Square():
-    """A square class."""
-
-    def __init__(self, size=0):
-        """Initialize class."""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-
-    def area(self):
-        """Return the current square area."""
-        return self.__size ** 2
-'''
-#!/usr/bin/python3
 """
     3-square.py
     Module defining square
@@ -37,10 +19,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    
+
     def area(self):
-        """function that calculates and returns square area.
-           return area
-        """
+        """Return the current square area."""
         return self.__size ** 2
-'''
