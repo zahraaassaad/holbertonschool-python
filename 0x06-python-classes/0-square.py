867d356021e45f958cca619778a63fe1ce41ea30
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-15 07:20:41 +0000

    solution

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,8 +2,19 @@
 
 
 class Square():
+    """A square class."""
+
+    def __init__(self):
+        """Initialize class."""
+        pass
+'''
+#!/usr/bin/python3
+
+
+class Square():
     """Square class."""
 
     def __init__(self):
         """To initialize the class."""
         pass
+'''
