00111ad3d88b47508a1e6156ee282dc01074c084
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-18 14:38:05 +0000

    all

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    3-say_my_name.py
+    This module contains the following functions:
+    - say_my_name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Print "My name is <first name> <last name>".
+    """
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    if not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
+    print("My name is {} {}".format(first_name, last_name))
---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    4-print_square.py
+    This module contains the following functions:
+    - print_square
+"""
+
+
+def print_square(size):
+    """
+    Print a square with the character #.
+    """
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    if size == 0:
+        return
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
