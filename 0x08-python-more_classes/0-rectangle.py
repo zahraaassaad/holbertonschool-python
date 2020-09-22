d66495df0faa6f2f0b6ea3e9782598f15fbe2385
zahraaassaad <zahraa.asaad8@gmail.com>
2020-09-22 09:38:38 +0000

    0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    0-square.py
+    Module defining square
+    return {}
+"""
+
+
+class Rectangle:
+    """Square class.
+       It defines a square and returns an empty block.
+    """
+    pass
