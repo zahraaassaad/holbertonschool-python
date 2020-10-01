443522f7256b845ac247bf56b02d07bc9a4e6f9a
zahraaassaad <zahraa.asaad8@gmail.com>
2020-10-01 17:29:39 +0000

    9

--- 0x0C-python-input_output/9-add_item.py
+++ 0x0C-python-input_output/9-add_item.py
@@ -7,6 +7,7 @@ Add all arguments to a Python list, and then save them to a file.
 import sys
 import os.path
 
+
 args = sys.argv[1:]
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
