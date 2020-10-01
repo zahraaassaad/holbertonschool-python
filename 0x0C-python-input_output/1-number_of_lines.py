7d3fe75f6c28a445caa6ae717a332375703c8a82
zahraaassaad <zahraa.asaad8@gmail.com>
2020-10-01 15:27:51 +0000

    1

--- 0x0C-python-input_output/1-number_of_lines.py
+++ 0x0C-python-input_output/1-number_of_lines.py
@@ -6,5 +6,6 @@ Module for number_of_lines.
 
 
 def number_of_lines(filename=""):
+    """Return the number of lines of a text file."""
     with open(filename, 'r') as f:
         return(len(list(f)))
