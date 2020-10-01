ddd52982c04b7bf529c670b434b1f88d0ad2bd96
zahraaassaad <zahraa.asaad8@gmail.com>
2020-10-01 16:10:59 +0000

    3

--- 0x0C-python-input_output/3-write_file.py
+++ 0x0C-python-input_output/3-write_file.py
@@ -8,5 +8,5 @@ Module for write_file.
 def write_file(filename="", text=""):
     """Writes a string to a text file (UTF8) and returns the number of characters written."""
     with open(filename, 'w') as f:
-        f.write(text)
-    return(len(text))
+        chars=f.write(text)
+    return(chars)
