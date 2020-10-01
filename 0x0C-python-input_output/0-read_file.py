15083304c8e2ae4be7160e290a5322b0842825ad
zahraaassaad <zahraa.asaad8@gmail.com>
2020-10-01 15:22:13 +0000

    comments added

--- 0x0C-python-input_output/0-read_file.py
+++ 0x0C-python-input_output/0-read_file.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
+"""
+Module for read_file.
+"""
+
 
 def read_file(filename=""):
+    "reads a text file (UTF8) and prints it to stdout."
     with open(filename, 'r') as f:
         for line in f:
             print(line, end='')
