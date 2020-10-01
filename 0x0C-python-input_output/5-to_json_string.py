6bbb5db43f13475abdfcf70b0bbd62d133ec299a
zahraaassaad <zahraa.asaad8@gmail.com>
2020-10-01 16:38:07 +0000

    dumps

--- 0x0C-python-input_output/5-to_json_string.py
+++ 0x0C-python-input_output/5-to_json_string.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import json
 
 """
 This is a module for to_json_string.
 """
 
+import json
+
 
 def to_json_string(my_obj):
     """Return the JSON representation of an object (string)."""
-    return(json.JSONEncoder().encode(my_obj))
+    return json.dumps(my_obj)
