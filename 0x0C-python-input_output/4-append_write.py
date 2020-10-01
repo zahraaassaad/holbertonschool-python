61d2cf555b38cb43c15081e53ea2f33173141dab
zahraaassaad <zahraa.asaad8@gmail.com>
2020-10-01 16:22:53 +0000

    capital and no s and line

--- 0x0C-python-input_output/4-append_write.py
+++ 0x0C-python-input_output/4-append_write.py
@@ -6,8 +6,8 @@ This is a module for append_write.
 
 
 def append_write(filename="", text=""):
-    """appends a string at the end of a text file (UTF8) 
-    and returns the number of characters added"""
+    """Append a string at the end of a text file (UTF8) and return the 
+    number of characters added."""
     with open(filename, 'a') as f:
         chars = f.write(text)
     return chars
