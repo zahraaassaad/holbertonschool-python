a6e2e37e5bf2446ad9909665e3b988143e0b220f
zahraaassaad <zahraa.asaad8@gmail.com>
2020-10-13 12:39:06 +0000

    removed comments

--- 0x0E-minimum_operations/0-minoperations.py
+++ 0x0E-minimum_operations/0-minoperations.py
@@ -9,7 +9,7 @@ def minOperations(n):
     """calculates the fewest number of operations needed."""
     res = 0
     for i in range(2, n):
-        while n%i == 0: #check if problem can be broken into smaller problem
-            res += i #if yes then add no of smaller problems to result. If number = 25  i = 5 then 5*5 = 25 so add 5 to results
-            n = n/i #create smaller problem
+        while n%i == 0:
+            res += i
+            n = n/i
     return res
