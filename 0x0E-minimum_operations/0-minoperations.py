49d7021526598055e4a801455d89308451630995
zahraaassaad <zahraa.asaad8@gmail.com>
2020-10-13 14:49:35 +0000

    start from 2

--- 0x0E-minimum_operations/0-minoperations.py
+++ 0x0E-minimum_operations/0-minoperations.py
@@ -8,7 +8,7 @@ Module for minOperations.
 def minOperations(n):
     """calculates the fewest number of operations needed."""
     res = 0
-    for i in range(n+1):
+    for i in range(2, n+1):
         while n % i == 0:
             res += i
             n = n/i
