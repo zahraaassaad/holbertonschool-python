761aef827b5ab6f67c4092f55b6465a650533392
zahraaassaad <zahraa.asaad8@gmail.com>
2020-10-13 12:52:00 +0000

    remove comments

--- 0x0E-minimum_operations/0-minoperations.py
+++ 0x0E-minimum_operations/0-minoperations.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
 
-"""
-Module for minOperations.
-"""
-
 
 def minOperations(n):
-    """calculates the fewest number of operations needed."""
     res = 0
     for i in range(2, n):
         while n%i == 0:
             res += i
             n = n/i
-    if n != 1:
-        return 0
     return res
