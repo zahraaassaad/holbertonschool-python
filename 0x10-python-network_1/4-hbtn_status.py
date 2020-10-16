1984da30660e53e49b5c8e540fe38918e0baefc0
zahraaassaad <zahraa.asaad8@gmail.com>
2020-10-16 14:15:42 +0000

    format

--- 0x10-python-network_1/4-hbtn_status.py
+++ 0x10-python-network_1/4-hbtn_status.py
@@ -9,5 +9,5 @@ import request
 if __name__ == "__main__":
         response = requests.get("https://intranet.hbtn.io/status")
         print("Body response:")
-        print("\t- type: {}".format(type(response)))
-        print("\t- content: {}".format(response))
+        print("\t- type: {}".format(type(response.text)))
+        print("\t- content: {}".format(response.text))
