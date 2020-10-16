efd10f88277dddffe6f3e3c8cf4a9d6ca90e1091
zahraaassaad <zahraa.asaad8@gmail.com>
2020-10-16 13:36:34 +0000

    redone

--- 0x10-python-network_1/2-post_email.py
+++ 0x10-python-network_1/2-post_email.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 """
-takes in a URL and an email, sends a POST request to the passed URL with the 
-email as a parameter, and displays the body of the response
+Takes in a URL and an email, sends a POST request to the passed URL with the
+email as a parameter, and displays the body of the response (decoded in utf-8)
 """
 
 import urllib.request
