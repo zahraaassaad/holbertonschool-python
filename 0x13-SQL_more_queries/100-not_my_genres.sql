3cf77ac154bf5a480fda90fde14b542ddee19106
zahraaassaad <zahraa.asaad8@gmail.com>
2020-10-23 14:20:49 +0000

    advanced

---
+++ 0x13-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,17 @@
+-- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+-- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+SELECT DISTINCT name
+     FROM tv_genres
+LEFT JOIN tv_show_genres
+       ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows
+       ON tv_show_genres.show_id = tv_shows.id
+    WHERE name NOT IN (
+    SELECT name
+      FROM tv_genres
+     LEFT JOIN tv_show_genres
+            ON genre_id = tv_genres.id
+     LEFT JOIN tv_shows
+            ON show_id = tv_shows.id
+	     WHERE title = "Dexter")
+ ORDER BY name ASC;
