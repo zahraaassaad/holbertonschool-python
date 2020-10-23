6b87c8bab2c92636d9e2a014d6f1426b1a889dfe
zahraaassaad <zahraa.asaad8@gmail.com>
2020-10-23 12:21:50 +0000

    0

--- 0x13-SQL_more_queries/0-privileges.sql
+++ 0x13-SQL_more_queries/0-privileges.sql
@@ -1,7 +1,7 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2
 -- list privileges for user_0d_1
 -- list privileges for user_0d_2
-SHOW GRANTS
+   SHOW GRANTS
  FOR user_0d_1@localhost;
-SHOW GRANTS
+     SHOW GRANTS
  FOR user_0d_2@localhost;
