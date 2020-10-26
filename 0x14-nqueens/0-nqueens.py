4d156ea970bbc544d891bc208551653a9b6111d5
zahraaassaad <zahraa.asaad8@gmail.com>
2020-10-26 15:49:05 +0000

    pep8)

--- 0x14-nqueens/0-nqueens.py
+++ 0x14-nqueens/0-nqueens.py
@@ -12,19 +12,23 @@ except ValueError:
 if N < 4:
     print("N must be at least 4")
     exit(1)
+
+
 def under_attack(col, queens):
     return col in queens or \
-           any(abs(col - x) == len(queens)-i for i,x in enumerate(queens))
+           any(abs(col - x) == len(queens)-i for i, x in enumerate(queens))
+
 
 def solve(n):
     solutions = [[]]
     for row in range(n):
         solutions = [solution+[i+1]
-                       for solution in solutions
-                       for i in range(BOARD_SIZE)
-                       if not under_attack(i+1, solution)]
+                     for solution in solutions
+                     for i in range(BOARD_SIZE)
+                     if not under_attack(i+1, solution)]
     return solutions
 
 if __name__ == '__main__':
     BOARD_SIZE = int(argv[1])
-    for answer in solve(BOARD_SIZE): print(list((x-1, y-1) for x,y in enumerate(answer, start=1)))
+    for answer in solve(BOARD_SIZE):
+        print(list((x-1, y-1) for x, y in enumerate(answer, start=1)))
