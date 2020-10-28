15e228bd3e5fe647699250bd2b342199df9572ae
zahraaassaad <zahraa.asaad8@gmail.com>
2020-10-28 15:04:23 +0000

    redone

--- 0x15-MySQL_Advanced/3-glam_rock.sql
+++ 0x15-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,4 @@
 -- lists all bands with Glam as their main style
-SELECT DISTINCT 'brand_name', 
-       		IFNULL ('split', 2020) - 'formed' as 'lifespan'
-       FROM 'metal_bands' WHERE FIND_IN_SET('Glam rock', style)
-       ORDER BY 'lifespan' DESC;
+SELECT brand_name, ifnull(split, 2020) - ifnull(formed,0) AS lifespan
+       FROM metal_bands WHERE style LIKE "%Glam rock%"
+       ORDER BY lifespan DESC;
