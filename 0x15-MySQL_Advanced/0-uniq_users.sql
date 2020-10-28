945bfb436ae84a59d3f9fec81649f3353bc6c909
zahraaassaad <zahraa.asaad8@gmail.com>
2020-10-28 13:56:18 +0000

    ,

--- 0x15-MySQL_Advanced/0-uniq_users.sql
+++ 0x15-MySQL_Advanced/0-uniq_users.sql
@@ -2,5 +2,5 @@
 CREATE TABLE IF NOT EXISTS users(
        id INT UNIQUE NOT NULL AUTO_INCREMENT,
        EMAIL CHAR(255) UNIQUE NOT NULL,
-       name CHAR(255)
+       name CHAR(255),
        PRIMARY KEY (ID));
