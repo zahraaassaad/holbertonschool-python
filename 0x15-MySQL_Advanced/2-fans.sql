771cb9b05b6d3b2a26f4347c74bfc3e1967b9157
zahraaassaad <zahraa.asaad8@gmail.com>
2020-10-28 14:56:00 +0000

    non distinct

--- 0x15-MySQL_Advanced/2-fans.sql
+++ 0x15-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- ranks country origins of bands,
 -- ordered by the number of (non-unique) fans
-SELECT DISTINCT 'origin', SUM('fans') as 'nb_fans' FROM 'metal_bands'
+SELECT 'origin', SUM('fans') AS 'nb_fans' FROM 'metal_bands'
 GROUP BY 'origin'
 ORDER BY 'nb_fans' DESC;
