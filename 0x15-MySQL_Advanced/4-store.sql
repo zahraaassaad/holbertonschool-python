3e4e2e5714801d8207e6f8629252513312c2045e
zahraaassaad <zahraa.asaad8@gmail.com>
2020-10-28 15:14:50 +0000

    4

---
+++ 0x15-MySQL_Advanced/4-store.sql
@@ -0,0 +1,10 @@
+-- creates a trigger that decreases the quantity
+-- of an item after adding a new order
+
+CREATE TRIGGER 
+reduce_quantity
+AFTER INSERT ON orders 
+FOR EACH ROW 
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
