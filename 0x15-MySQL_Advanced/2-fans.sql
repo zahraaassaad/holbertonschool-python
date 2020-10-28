75f8f87adbe6ab976f22e404a82417f2bd80f057
zahraaassaad <zahraa.asaad8@gmail.com>
2020-10-28 14:41:36 +0000

    syntax

--- 0x15-MySQL_Advanced/2-fans.sql
+++ 0x15-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
 -- ranks country origins of bands, ordered by the number of (non-unique) fans
-SELECT DISTINCT 'origin', SUM('fans') AS nb_fans FROM 'METAL_BANDS' 
+SELECT DISTINCT 'origin', SUM('fans') AS 'nb_fans' FROM 'metal_bands' 
        GROUP BY 'origin'
-       ORDER BY nb_fans
+       ORDER BY 'nb_fans' DESC;
--- 0x15-MySQL_Advanced/3-glam_rock.sql
+++ 0x15-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- lists all bands with Glam as their main style
-select 'brand_name', 
-       IFNULL ('split', 2020) - 'formed' as 'lifespan'
+SELECT DISTINCT 'brand_name', 
+       		IFNULL ('split', 2020) - 'formed' as 'lifespan'
        FROM 'metal_bands' WHERE FIND_IN_SET('Glam rock', style)
        ORDER BY 'lifespan' DESC;
