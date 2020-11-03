222ec6062c1028e7ec1c1d13c82926154e24d389
zahraaassaad <zahraa.asaad8@gmail.com>
2020-11-03 14:25:39 +0000

    manda

---
+++ 0x17-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""pymongo search function"""
+
+def schools_by_topic(mongo_collection, topic):
+    """returns the list of school having a specific topic"""
+    return mongo_collection.find({ "topics": topic })
