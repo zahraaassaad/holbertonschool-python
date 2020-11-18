a924bf01a428507f78c28ef06edb5d0404ca1359
zahraaassaad <zahraa.asaad8@gmail.com>
2020-11-18 11:02:44 +0000

    redone

--- 0x1C-Unittests_and_integration_tests/test_client.py
+++ 0x1C-Unittests_and_integration_tests/test_client.py
@@ -1,36 +1,26 @@
 #!/usr/bin/env python3
-''' Test client.py '''
+""" Unittest module """
 
 from unittest import TestCase, mock
+from unittest.mock import patch, Mock
 from parameterized import parameterized
+
 import client
 from client import GithubOrgClient
 
 
 class TestGithubOrgClient(TestCase):
-    ''' Test class GithubOrgClient '''
+    """ Class for testing GithubOrgClient """
 
     @parameterized.expand([
-        ('buttercup'),
-        ('verne'),
-        ('koi')
+        ("google"),
+        ("abc"),
     ])
-    @mock.patch('client.get_json')
-    def test_org(self, org_name, mock_get_json):
-        ''' Test GithubOrgClient.org() '''
-
-        goc = client.GithubOrgClient(org_name)
-
-        goc.org()
-        mock_get_json.assert_called_once_with(
-            'https://api.github.com/orgs/{}'.format(org_name))
-
-    def test_public_repos_url(self):
-        ''' Test GithubOrgClient.public_repos_url property. '''
-
-        with mock.patch('client.GithubOrgClient._public_repos_url', new_callable=mock.PropertyMock) as m:
-            m.return_value = 'test'
-
-            goc = GithubOrgClient('buttercup')
-            pru = goc._public_repos_url
-            self.assertEqual(pru, 'test')
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_json):
+        """ Test method returns correct output """
+        gc = GithubOrgClient(org_name)
+        gc.org()
+        mock_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}"
+            )
