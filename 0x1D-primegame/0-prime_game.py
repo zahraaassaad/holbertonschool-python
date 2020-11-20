6d9be37775d37de3e775001ce9742cdb5ae4424b
zahraaassaad <zahraa.asaad8@gmail.com>
2020-11-20 17:49:08 +0000

    redone

--- 0x1D-primegame/0-prime_game.py
+++ 0x1D-primegame/0-prime_game.py
@@ -7,12 +7,33 @@ Module for isWinner.
 
 def isWinner(x, nums):
     """ Prime numbers game. """
-    biggest_prime = nums[-1]
-    if biggest_prime < 1 or x < 0:
+    if x < 0 or nums[-1] < 1:
         return None
-    if x > biggest_prime:
-        for i in range(biggest_prime + 1, x + 1):
-            for y in range(2, int(x ** .5) + 1):
-                if x % y:
-                    nums.append(i)
-    return 'Ben' if sum(i <= x for i in nums) % 2 else 'Maria'
+    Alice_score = 0
+    Bob_score = 0
+    prime = [False] * 100001
+    primes = []
+    for i in range(2, 100001):
+        if not prime[i]:
+            primes.append(i)
+            for j in range(i, 100001, i):
+                prime[j] = True
+
+    l = 0
+    for a0 in range(x):
+        n = nums[l]
+        # your code goes here
+        num = 0
+        for i in range(100001):
+            if i >= len(primes) or primes[i] > n:
+                break
+            num += 1
+        l += 1
+        if (['Maria', 'Ben'][(num % 2) ^ 1]) == "Maria":
+            Alice_score += 1
+        else:
+            Bob_score += 1
+    if Alice_score > Bob_score:
+        return "Maria"
+    else:
+        return "Ben"
--- 0x1D-primegame/copy.py
+++ 0x1D-primegame/copy.py
@@ -8,11 +8,8 @@ Module for isWinner.
 def isWinner(x, nums):
     """ Prime numbers game. """
     biggest_prime = nums[-1]
-    if biggest_prime < 1 or x < 1:
+    if biggest_prime < 1 or x < 0:
         return None
-    for l in nums:
-        if l < 1:
-            return None
     if x > biggest_prime:
         for i in range(biggest_prime + 1, x + 1):
             for y in range(2, int(x ** .5) + 1):
