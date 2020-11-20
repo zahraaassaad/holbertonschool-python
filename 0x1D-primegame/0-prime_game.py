2f803a41d7f987f8924a4cf1dc09d8c2441603d3
zahraaassaad <zahraa.asaad8@gmail.com>
2020-11-20 16:48:43 +0000

    0

--- 0x1D-primegame/0-prime_game.py
+++ 0x1D-primegame/0-prime_game.py
@@ -8,7 +8,7 @@ Module for isWinner.
 def isWinner(x, nums):
     """ Prime numbers game. """
     biggest_prime = nums[-1]
-    if biggest_prime < 1 or x < 0:
+    if biggest_prime < 1 or x < 2:
         return None
     if x > biggest_prime:
         for i in range(biggest_prime + 1, x + 1):
