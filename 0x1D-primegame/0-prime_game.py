33b484deed4328a87c5ebebc2a61894fa1141740
zahraaassaad <zahraa.asaad8@gmail.com>
2020-11-20 16:34:40 +0000

    negative rounds

--- 0x1D-primegame/0-prime_game.py
+++ 0x1D-primegame/0-prime_game.py
@@ -8,7 +8,7 @@ Module for isWinner.
 def isWinner(x, nums):
     """ Prime numbers game. """
     biggest_prime = nums[-1]
-    if biggest_prime < 1:
+    if biggest_prime < 1 or x < 1:
         return None
     if x > biggest_prime:
         for i in range(biggest_prime + 1, x + 1):
